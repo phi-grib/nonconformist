880380fb9ab58745c7f74667dd9812850fa2168d
Manuel Pastor <manuel.pastor@upf.edu>
2022-07-13 19:03:58 +0200

    small change to make sure the probabilities are reproducible

--- nonconformist/util.py
+++ nonconformist/util.py
@@ -2,6 +2,12 @@ from __future__ import division
 import numpy as np
 
 def calc_p(ncal, ngt, neq, smoothing=False):
+
+	# MP: use a reproducible random seed to assign exactly the same probabilities
+	# to the same object
+	seed= np.int32((ncal+ngt+neq))
+	np.random.seed(seed)
+
 	if smoothing:
 		return (ngt + (neq + 1) * np.random.uniform(0, 1)) / (ncal + 1)
 	else:
